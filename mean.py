05466ea4698ac5f87a4d42fde9cdb41c2d531b9b
OkeWoke <toamumbo@gmail.com>
2018-08-24 21:12:04 +1200

    Added averaging of datapoints code, residual plotting

--- mean.py
+++ mean.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import time, re, glob, os
 
-DIRECTORY = "C:\\Users\\Liam\\Desktop\\LinearityTest\\LIGHT" #Directory containing all image files
+DIRECTORY = "C:\\Users\\Liam\\ownCloud\\LinearityTest\\LIGHT" #Directory containing all image files
 
 def timer(func):
     """Takes a function, returns the time duration for execution of the function and any output from function"""
@@ -34,8 +34,15 @@ expTimeArray = []
 os.chdir(DIRECTORY)
 for file in glob.glob("*.tif"):
     image = Image.open(file)    
-    meanArray.append(mean(image))
-    expTimeArray.append(getExposure(file))
+    expTime = getExposure(file)
+    meanADU  = mean(image)
+    if expTime in expTimeArray: #if already added datapoint for a given exp time, avg it.
+        i = expTimeArray.index(expTime)
+        meanArray[i]+=meanADU
+        meanArray[i]/=2
+    else:#else add new entries into arrays.
+        meanArray.append(meanADU)
+        expTimeArray.append(expTime)
     
 #y=mx+c
 #Slope, Intercept, correlation coef, pval for hyp test, standard error
@@ -44,15 +51,26 @@ m, c, r_value, p_value, se = stats.linregress(expTimeArray,meanArray)
 xLinSpace= np.linspace(0,300,100)
 
 yFit = m*xLinSpace + c
-print(m,c)
+plt.figure(0)
 plt.xlabel("Exposure Time (s)")
-plt.ylabel("Mean Pixel Value (au)")
+plt.ylabel("Mean Pixel Value ADU")
 plt.title("Linearity of Canon 60D")
 
 plt.plot(expTimeArray,meanArray,marker='.',linestyle="None")
 plt.plot(xLinSpace,yFit,linestyle="solid")
 
-#plt.plot(xLinSpace,yFit+2*se,color="r",linestyle="dotted")#2SE lines
-#plt.plot(xLinSpace,yFit-2*se,color="r",linestyle="dotted")
-print(r_value,p_value,se)
+print("R-Square: ",r_value**2)
+print("ADU/Sec: ",m)
+print("intercept: ",c)
 plt.savefig("plot.pdf")
+
+residuals = []
+for i in range(0,len(expTimeArray)):
+    residuals.append((((meanArray[i]-c)/expTimeArray[i])-m)/m)
+    
+plt.figure(1)
+plt.title("Residual Deviation")
+plt.xlabel("Exposure Time (s)")
+plt.ylabel("Deviation %")
+plt.plot(expTimeArray[:-1],residuals[:-1])
+plt.savefig("plot2.pdf")
